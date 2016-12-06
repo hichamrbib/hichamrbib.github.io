bc4b036e52de9b3c5cff83f97dc195aff0bf8a46
Hicham Rbib <contact@kritiqart.com>
2016-12-06 23:20:27 +0000

    add directionnel survol

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
+/***********  firstWrap section  *********/
 	$height = $(window).height();
-	if ($(window).width() >= 768 && $height >= 662) 
-	{
-		$height = $height - $height/4;
-	}
-
+	$width 	= $(window).width();
+	if ($width >= 768 && $height >= 638) 
+	{$height = $height - $height/5;}
 	$(".firstWrap").css('height', $height + 'px');
 
 /********** Sticky Navigation **********/
@@ -18,5 +17,30 @@ var navOffset = $("nav").offset().top;
 		}
 	});
 /***************************************/
+
+/***********  survol directionnel *******************/
+	function _getDir($el, event){
+		var	w  	= $el.width();
+			h   = $el.height();
+			cx  = $el.offset().left + w/2,
+			cy  = $el.offset().top + h/2,
+			x   = (event.pageX - cx) * (w>h?(h/w) : 1),
+			y   = -(event.pageY - cy) * (h>w?(w/h) : 1);
+			direction = Math.round( ( (Math.atan2(x,y) + Math.PI) / (Math.PI/2)) + 2 ) % 4 
+		var directions = {
+				0 : {left:0, top:-h},
+				1 : {left:w, top:0},
+				2 : {left:0, top:h},
+				3 : {left:-w, top:0}
+			}
+		return directions[direction];
+	}
+	$('.thumb').on('mouseenter', function(event){
+		$(this).find('.alt').css(_getDir($(this),event)).animate({top: 0, left: 0}, 300);
+	});
+
+	$('.thumb').on('mouseleave', function(event){
+		$(this).find('.alt').animate(_getDir($(this),event), 300);
+	});
 
 });
